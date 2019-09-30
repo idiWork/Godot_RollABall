9c040e181ed0a50faa2d49cc65cec91cf82f2c4b
Elena Guzman Blanco <elena.guzbla@gmail.com>
2019-09-30 14:13:12 +0200

    Add ball node and movement script

---
+++ idiWork_Car3D/scripts/Ball.gd
@@ -0,0 +1,21 @@
+extends KinematicBody
+
+var movement = Vector3()
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	
+	movement.y -= 1
+	
+	if Input.is_action_pressed("ui_up"):
+		movement.z += 0.5
+	elif Input.is_action_pressed("ui_down"):
+		movement.z -= 0.5
+	elif Input.is_action_pressed("ui_left"):
+		movement.x -= 0.5
+	elif Input.is_action_pressed("ui_right"):
+		movement.x += 0.5
+	
+	move_and_slide(movement)
